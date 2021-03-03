3db6d13e5550b339001e0e4ce1bedd2b396236f2
saouk <lna1003@naver.com>
2021-03-03 13:52:59 +0900

    first Setting

---
+++ src/main/java/com/shop/online/shopingMall/ShopingMallApplication.java
@@ -0,0 +1,13 @@
+package com.shop.online.shopingMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopingMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/online/shopingMall/ShopingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.online.shopingMall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
