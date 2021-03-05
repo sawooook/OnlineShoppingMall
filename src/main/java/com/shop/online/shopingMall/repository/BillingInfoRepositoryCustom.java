0df9bb51c4bc9541ab0991ed39ee61e32037d0a2
saouk <lna1003@naver.com>
2021-03-05 11:51:41 +0900

    query dsl 플러그인 추가

---
+++ src/main/java/com/shop/online/shopingMall/repository/BillingInfoRepositoryCustom.java
@@ -0,0 +1,9 @@
+package com.shop.online.shopingMall.repository;
+
+import com.shop.online.shopingMall.domain.BillingInfo;
+
+public interface BillingInfoRepositoryCustom {
+
+    BillingInfo activeBillingInfo(BillingInfo id);
+
+}
---
+++ src/main/java/com/shop/online/shopingMall/repository/BillingInfoRepositoryImpl.java
@@ -0,0 +1,25 @@
+//package com.shop.online.shopingMall.repository;
+//
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//import com.shop.online.shopingMall.domain.BillingInfo;
+//
+//import javax.persistence.EntityManager;
+//
+//public class BillingInfoRepositoryImpl implements BillingInfoRepositoryCustom {
+//
+//    private final JPAQueryFactory queryFactory;
+//
+//    public BillingInfoRepositoryImpl(EntityManager em) {
+//        this.queryFactory = new JPAQueryFactory(em);
+//    }
+//
+//    @Override
+//    public BillingInfo activeBillingInfo(BillingInfo id) {
+//        queryFactory.select(BillingInfo.f)
+//
+//
+//
+//        return null;
+//
+//    }
+//}
